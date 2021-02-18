72dbabab1c3296515e8474e2e57a6ba678abe784
michalOch <ochrobot1990@gmail.com>
2021-02-18 20:45:58 +0100

    lesson 1

---
+++ app.js
@@ -0,0 +1,38 @@
+/*
+  multi 
+  line 
+  comments
+*/
+
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeding = 'Hello';
+console.log(greeding);
+console.log(1, 23, 4, 5, 6);
+console.log([1, 2, 4, 5, 6, 3, 2, 1]);
+
+console.log({
+  a: 1,
+  b: 2
+});
+
+console.table({
+  a: 1,
+  b: 2
+});
+
+console.error('This is some error');
+// console.clear();
+console.warn('This is a warning');
+console.info('This is a info');
+
+console.time('Hello');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd('Hello');
