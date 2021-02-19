d65c2f3aadc84330952c3484e8ce05720f3e448a
michalOch <ochrobot1990@gmail.com>
2021-02-19 12:19:09 +0100

    lesson 3 - type conversion

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
-// PRIMITIVES TYPES
+let val;
 
-// String
-const name = 'John Doe';
-console.log(typeof name);
-// Number
-const age = 45;
-console.log(typeof age);
-// Boolean
-const hasKids = true;
-console.log(typeof hasKids);
-// Null
-const car = null;
-console.log(typeof car);
-// Undefined
-let test;
-console.log(typeof test);
-// Symbol
-const sym = Symbol();
-console.log(typeof sym);
+// Number to string
+val = String(555);
+val = String(4 + 41);
+// Bool to string
+val = String(true);
+// Date to string
+val = String(new Date());
+// Array to string
+val = String([1, 2, 3, 4, 5, 6]);
+// toString()
+val = (5).toString();
+val = (true).toString();
 
-// REFERENCE TYPES - Objects
-// Array
-const hobbies = ['movies', 'music', 'running'];
-console.log(typeof hobbies);
-// Object literals
-const addres = {
-  city: 'Boston',
-  state: 'MA',
-  country: 'USA'
-}
-console.log(typeof addres);
-// dates
-const today = new Date();
-console.log(today);
-console.log(typeof today);
+// String to number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val - Number(null);
+val = Number('hello');
+val = Number([1, 2, 3]);
+// parseInt
+val = parseInt('120');
+val = parseFloat('120.53');
+
+
+// Output
+// console.log(val);
+// console.log(typeof val);
+// only works on strings
+// console.log(val.length); 
+// only works on number
+// console.log(val.toFixed(2));
+
+const val1 = '5';
+const val2 = 6;
+const sum = parseInt(val1) + val2;
+console.log(sum);
+console.log(typeof sum);
