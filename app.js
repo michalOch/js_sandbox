cf21b7d5ecb15f03569f407a5501011c52dd3ad8
michalOch <ochrobot1990@gmail.com>
2021-02-19 14:58:24 +0100

    lesson 4 - maths

--- app.js
+++ app.js
@@ -1,40 +1,27 @@
+const num1 = 100;
+const num2 = 50;
 let val;
 
-// Number to string
-val = String(555);
-val = String(4 + 41);
-// Bool to string
-val = String(true);
-// Date to string
-val = String(new Date());
-// Array to string
-val = String([1, 2, 3, 4, 5, 6]);
-// toString()
-val = (5).toString();
-val = (true).toString();
+// Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 - num2;
+val = num1 % num2;
 
-// String to number
-val = Number('5');
-val = Number(true);
-val = Number(false);
-val - Number(null);
-val = Number('hello');
-val = Number([1, 2, 3]);
-// parseInt
-val = parseInt('120');
-val = parseFloat('120.53');
+// Math objects
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.pow(8, 2);
+val = Math.abs(-54);
+val = Math.min(2, 33, 44, 21, 11, 55, -1);
+val = Math.max(2, 5, 21, 6, 1, 1, 122, 213412);
+val = Math.random();
+val = Math.floor(Math.random() * 20 + 1);
 
-
-// Output
-// console.log(val);
-// console.log(typeof val);
-// only works on strings
-// console.log(val.length); 
-// only works on number
-// console.log(val.toFixed(2));
-
-const val1 = '5';
-const val2 = 6;
-const sum = parseInt(val1) + val2;
-console.log(sum);
-console.log(typeof sum);
+// output 
+console.log(val);
