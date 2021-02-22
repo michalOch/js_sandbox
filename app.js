b2406ab012740996f6ec63d5d7f588f2e7c431a1
michalOch <ochrobot1990@gmail.com>
2021-02-22 19:11:16 +0100

    functions

--- app.js
+++ app.js
@@ -1,46 +1,61 @@
-const color = 'red';
+// FUNCTION DECLARATIONS
+/********************************************** */
 
-switch (color) {
-  case 'red':
-    console.log('Its red');
-    break;
-  case 'blue':
-    console.log('Its blue');
-    break;
-  case 'blue':
-    console.log('Its blue');
-    break;
-  default:
-    console.log("Other color");
-    break;
+// before ES6
+// function greet(firstName, lastname) {
+//   if (typeof firstName === 'undefined') {
+//     firstName = 'John';
+//   }
+//   if (typeof lastname === 'undefined') {
+//     lastname = 'Doe';
+//   }
+//   return `Hello ${firstName} ${lastname}`;
+// }
+
+//after ES6
+function greet(firstName = 'John', lastname = 'Doe') {
+  // console.log('Hello');
+  return `Hello ${firstName} ${lastname}`;
 }
 
-let day;
-switch (new Date().getDay()) {
-  case 0:
-    day = 'Sunday';
-    break;
-  case 1:
-    day = 'Monday';
-    break;
-  case 2:
-    day = 'Tuesday';
-    break;
-  case 3:
-    day = 'Wednesday';
-    break;
-  case 4:
-    day = 'Thursday';
-    break;
-  case 5:
-    day = 'Friday';
-    break;
-  case 6:
-    day = 'Saturday';
-    break;
-  default:
-    day = 'Undefined';
-    break;
+// console.log(greet('Mark', 'Hamilton'));
+// console.log(greet());
+
+// FUNCTION EXPRESSIONS
+/********************************************** */
+const square = function (x = 1) {
+  return x * x;
+};
+
+// console.log(square());
+// console.log(square(10));
+
+// IMMEDIATELLY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+/********************************************** */
+
+// (function () {
+//   console.log('IIFE Ran..');
+// })();
+
+// (function (name) {
+//   console.log('Hello ' + name);
+// })('Patrick');
+
+// PROPERTY METHODS 
+/********************************************** */
+const todo = {
+  add: function () {
+    console.log('Add todo...');
+  },
+  edit: function (id) {
+    console.log(`Edit todo ${id}`);
+  }
 }
 
-console.log(`Today is ${day}`);
+todo.delete = function () {
+  console.log('Delete todo..');
+}
+
+todo.add();
+todo.edit(15);
+todo.delete();
